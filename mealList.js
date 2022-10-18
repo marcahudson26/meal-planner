51252e4414aeca3d92f5584441e43fec004bbf7b
Marc <marchudson2601@gmail.com>
2022-10-18 23:59:53 +0100

    removed spaces

--- mealList.js
+++ mealList.js
@@ -1,6 +1,6 @@
-const mealsList = [
+const mealslist = [
     {
-        name: 'Hotdogs',
+        name: 'hotdogs',
         ingredients: ['hot dog sausages ', 'hot dog sausages ', 'bread rolls', 'onion', 'tomato sauce', 'mustard'],
     },
     {
@@ -8,51 +8,51 @@ const mealsList = [
         ingredients: ['1kg mince', 'jar of red lasagne sauce', 'tomato passata', 'onion', 'italian herbs', 'garlic', 'beef stock cube', 'lasagne sheets', 'white sauce', 'garlic baguette','garlic baguette'],
     },
     {
-        name: 'Smoked fish',
+        name: 'smoked fish',
         ingredients: ['smoked fish', 'new potatoes', 'paprika', 'garlic granules', 'frozen sweetcorn'],
     },
     {
-        name: 'Marmite potatos',
+        name: 'marmite potatos',
         ingredients: ['potatoes', 'marmite', 'chicken kievs', 'frozen sweetcorn'],
     },
     {
-        name: 'Burgers',
+        name: 'burgers',
         ingredients: ['burgers', 'burger buns', 'burger sauce', 'gherkins', 'potatoes', 'carrots', 'red cabbage', 'white cabbage', 'red onion', 'mayo'],
     },
     {
-        name: 'Stew and dumplings',
+        name: 'stew and dumplings',
         ingredients: ['diced beef', 'mince', 'beef stock cube', 'gravy granuels', 'stew vegetable pack', 'potatoes', 'garlic', 'dumpling pack','dumpling pack'],
     },
     {
-        name: 'Pie and mash',
-        ingredients: ['Pies', 'potatoes', 'milk', 'butter', 'gravy granuels', 'Yorkshire puddings', 'frozen mushypeas']
+        name: 'pie and mash',
+        ingredients: ['pies', 'potatoes', 'milk', 'butter', 'gravy granuels', 'yorkshire puddings', 'frozen mushypeas']
     },
     {
-        name: 'Chicken roast',
-        ingredients: ['Large Whole Chicken', 'Stuffing', 'Gravy granuels', 'butternut squash', 'garlic', 'broccoli', 'carrots', 'leeks', 'garlic and herb soft cheese', 'double cream', 'honey', 'potatoes']
+        name: 'chicken roast',
+        ingredients: ['large whole chicken', 'stuffing', 'gravy granuels', 'butternut squash', 'garlic', 'broccoli', 'carrots', 'leeks', 'garlic and herb soft cheese', 'double cream', 'honey', 'potatoes']
     },
     {
         name: 'gammon eggs',
-        ingredients: ['Gammon joint', 'eggs', 'potatoes'],
+        ingredients: ['gammon joint', 'eggs', 'potatoes'],
     },
     {
         name: 'spaghetti bolognese',
-        ingredients: ['mince', 'onion', 'garlic', 'cheese', 'Bolognese sauce', 'tomato passata', 'Italian herbs', 'spaghetti pasta'],
+        ingredients: ['mince', 'onion', 'garlic', 'cheese', 'bolognese sauce', 'tomato passata', 'italian herbs', 'spaghetti pasta'],
     },
     {
         name: 'sausage and mash',
         ingredients: ['chipolata', 'potatoes', 'butter', 'milk', 'gravy granuels', 'red onion', 'red wine stock pot', 'green veg'],
     },
     {
-        name: 'Sausages pasta',
+        name: 'sausages pasta',
         ingredients: ['sausages', 'pasta', 'jar of pasta sauce', 'chopped tomatoes', 'italian herbs'],
     },
     {
-        name: 'bbq  honey  sausage',
-        ingredients: ['BBQ sauce', 'honey and pork sausages', 'new potatoes', 'garlic granules', 'frozen sweetcorn'],
+        name: 'bbq honey sausage',
+        ingredients: ['bbq sauce', 'honey and pork sausages', 'new potatoes', 'garlic granules', 'frozen sweetcorn'],
     },
     {
-        name: 'tuna  pasta  bake',
-        ingredients: ['tuna', 'Pasta bake packet', 'Milk', 'pasta', 'cheese'],
+        name: 'tuna pasta bake',
+        ingredients: ['tuna', 'pasta bake packet', 'milk', 'pasta', 'cheese'],
     },
 ]
